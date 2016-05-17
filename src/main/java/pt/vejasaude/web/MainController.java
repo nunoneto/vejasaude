26966d19db54818f970d673815f78a598c2333be
Nuno Neto <nuno.agn@gmail.com>
2016-05-17 22:22:52 +0100

    app init

---
+++ src/main/java/pt/vejasaude/web/MainController.java
@@ -0,0 +1,19 @@
+package pt.vejasaude.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Nuno on 17/05/2016.
+ */
+
+@RestController("")
+public class MainController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String home(){
+        return "Hello!!!";
+    }
+
+}
