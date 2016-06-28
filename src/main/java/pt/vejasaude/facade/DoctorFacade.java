b9b4f15faaa3690ea354b7c95f894d4a1c3d9060
Nuno Neto <nuno.neto@celfocus.com>
2016-06-28 22:07:29 +0100

    fix ramuk's pupu

--- src/main/java/pt/vejasaude/facade/DoctorFacade.java
+++ src/main/java/pt/vejasaude/facade/DoctorFacade.java
@@ -1,6 +1,7 @@
 package pt.vejasaude.facade;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pt.vejasaude.unified.data.doctor.Doctor;
 import pt.vejasaude.unified.data.doctor.IDoctor;
 import pt.vejasaude.web.services.doctor.request.CreateNewDoctorRequest;
@@ -8,6 +9,7 @@ import pt.vejasaude.web.services.doctor.request.CreateNewDoctorRequest;
 /**
  * Created by fmorais on 15/06/2016.
  */
+@Service
 public class DoctorFacade implements IDoctorFacade{
     @Autowired
     private IDoctor doctorDAO;
