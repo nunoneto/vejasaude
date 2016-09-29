d17c94be67a311e5a0b69cac52efa277ecaa9850
Nuno Neto <nuno.agn@gmail.com>
2016-09-29 23:06:05 +0100

    fix forgotten conflict

--- src/main/java/pt/vejasaude/web/services/doctor/response/CreateDoctorResponse.java
+++ src/main/java/pt/vejasaude/web/services/doctor/response/CreateDoctorResponse.java
@@ -1,13 +1,8 @@
 package pt.vejasaude.web.services.doctor.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import pt.vejasaude.unified.data.curriculumVitae.CurriculumVitae;
 import pt.vejasaude.unified.data.medicalSpecialty.MedicalSpecialty;
 import pt.vejasaude.unified.data.doctor.Doctor;
-<<<<<<< HEAD
-=======
-import pt.vejasaude.web.services.attachment.response.CreateNewAttachmentResponse;
->>>>>>> 792ed2448dadd5c2f280d34ce20e7c71fb08d289
 import pt.vejasaude.web.services.curriculumVitae.response.CreateNewCurriculumResponse;
 
 import java.io.Serializable;
