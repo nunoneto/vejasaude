b4de9a31cbc7810f058780c7f590978b6d11bbea
Mário Teixeira <1090626@isep.ipp.pt>
2016-10-26 10:30:51 +0100

    Implemented delete function of user.

--- src/main/resources/public/bo/app/app.js
+++ src/main/resources/public/bo/app/app.js
@@ -20,7 +20,7 @@ vejaSaudeBo.config(['$locationProvider', '$routeProvider','$httpProvider','ngToa
         }).when('/home/users', {
           controller:   'UserController',
           templateUrl:  'views/menus/users/list-users.html'
-        }).when('/user/:username/:mode', {
+        }).when('/user/:mode/:username?', {
           controller:   'CreateEditUserController',
           templateUrl:  'views/menus/users/edit-user.html'
         }).when('/home/doctors', {
--- src/main/resources/public/bo/app/controller/UserController.js
+++ src/main/resources/public/bo/app/controller/UserController.js
@@ -1,32 +1,40 @@
 (function(){
     angular
     .module("vejaSaudeBo")
-    .controller('UserController', ['$scope', '$location', 'UserService',
-        function($scope, $location, UserService){
+    .controller('UserController', ['$scope', '$location', 'UserService', 'dialogs',
+        function($scope, $location, UserService, dialogs){
         
             //Get all users
-            UserService.getAll().then(
-                function(data){
-                    $scope.users = data;
-                },
-                function(err){
-                    //show warning
-                }
-            );
+            var updateList = function(){
+                UserService.getAll().then(
+                    function(data){
+                        $scope.users = data;
+                    },
+                    function(err){
+                        //show warning
+                    }
+                );
+            }
+            updateList();
 
             //Create User
             $scope.createUser = function(user){
                 $location.path('/user/create');
             }
 
-            //Edit user
+            //Edit User
             $scope.editUser = function(user){
-                $location.path('/user/'+user.username+'/edit');
+                $location.path('/user/edit/'+user.username);
             }
 
-            //Delete User
+            //Eliminar User
             $scope.deleteUser = function(user){
-                $location.path('/user/'+user.username+'/delete')
+                //$location.path('/user/'+user.username+'/delete')
+                var confirmDialog = dialogs.confirm("Apagar Utilizador", "Tem a certeza que quer apagar o utilizador '" + user.prettyName + "'?", null);
+                confirmDialog.result.then(function(btn){
+                    UserService.delete(user.username);
+                    updateList();
+                });
             }
             
         }]);
